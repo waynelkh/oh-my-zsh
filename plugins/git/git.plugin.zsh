a7bf03ff0870153e1f1d77be321e4c14b181304c
ncanceill <nicolas.canceill@ens-cachan.org>
2015-04-04 22:27:58 +0200

    fix quotes in gwip alias

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -184,4 +184,4 @@ alias gupv='git pull -v --rebase'
 alias gvt='git verify-tag'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
-alias gwip="git add -A; git rm $(git ls-files --deleted); git commit -m \"--wip--\""
+alias gwip='git add -A; git rm $(git ls-files --deleted); git commit -m "--wip--"'
